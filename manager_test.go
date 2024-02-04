a0c5a497c722d5a337d8b006ce3eab8b5642d1cb
awalol <awalolcn@gmail.com>
2024-02-05 00:47:24 +0800

    support encrypted database

--- manager_test.go
+++ manager_test.go
@@ -7,11 +7,28 @@ import (
 )
 
 func TestNewManager(t *testing.T) {
-	mgr, err := NewManager("./testdata")
-	assert.NoError(t, err)
-	assert.NotNil(t, mgr)
+	t.Run("Default", func(t *testing.T) {
+		mgr, err := NewManager("./testdata")
+		assert.NoError(t, err)
+		assert.NotNil(t, mgr)
 
-	vault, err := mgr.OpenVault("")
-	assert.NoError(t, err)
-	assert.NotNil(t, vault)
+		vault, err := mgr.OpenVault("")
+		assert.NoError(t, err)
+		assert.NotNil(t, vault)
+	})
+	t.Run("Crypto", func(t *testing.T) {
+		mgr, err := NewManager("./testdata")
+		assert.NoError(t, err)
+		assert.NotNil(t, mgr)
+
+		vault, err := mgr.OpenVaultCrypto("crypto", "123456")
+		val, err := vault.GetString("world")
+		assert.NotNil(t, vault)
+
+		assert.Equal(t, "hello", val)
+		assert.NoError(t, err)
+
+		_, err = vault.GetBytes("foo")
+		assert.Error(t, err)
+	})
 }
